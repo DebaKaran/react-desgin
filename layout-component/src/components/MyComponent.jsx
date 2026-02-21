748cad304a38e637381b88c76b1b2a6d5683c5d9
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-21 14:51:52 +0530

    Without Layout Component Concept

---
+++ layout-component/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyComponent = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Component Code...</h1>
+    </div>
+  )
+}
+
+export default MyComponent
