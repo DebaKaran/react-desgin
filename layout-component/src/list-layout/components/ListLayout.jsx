6810d4fd92a3a96593e2cf03d389b94c51cfdf26
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-22 13:30:50 +0530

    By using $ in transition props, avoiding DOM leakage

--- layout-component/src/list-layout/components/ListLayout.jsx
+++ layout-component/src/list-layout/components/ListLayout.jsx
@@ -2,7 +2,7 @@ import { ListWrapper } from '../styled/ListLayout.styled'
 
 const ListLayout = ({ items, renderItem, variant = "grid", columns= 3}) => {
   return (
-    <ListWrapper variant={variant} columns={ columns}>
+    <ListWrapper $variant={variant} $columns={ columns}>
         {items.map((item) => (
             <div key={item.id}>
                 {renderItem(item)}
